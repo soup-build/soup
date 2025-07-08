c45eca7e50c4dc973512b52791d1ae39d30f67cb
Matthew Asplund <mwasplund@gmail.com>
2025-07-07 19:24:20 -0500

    Masplund/version (#328)
    * Update version

--- code/installer/soup-installer/Setup.cs
+++ code/installer/soup-installer/Setup.cs
@@ -7,7 +7,7 @@ public static class Script
 {
 	public static void Main()
 	{
-		var soupVersion = new Version(0, 42, 4);
+		var soupVersion = new Version(0, 42, 5);
 
 		var soupOutFolder = "../../../out";
 		var soupReleaseFolder = $"{soupOutFolder}/release";
