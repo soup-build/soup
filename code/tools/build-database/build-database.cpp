c29981b026f9cf2d2d508b17b5145d4ddd86a321
mwasplund <mwasplund@outlook.com>
2026-05-15 17:00:56 -0700

    Start loading the build database from the operation graph

--- code/tools/build-database/build-database.cpp
+++ code/tools/build-database/build-database.cpp
@@ -3,10 +3,14 @@ module;
 #include <filesystem>
 #include <format>
 #include <memory>
-#include <spanstream>
+#include <set>
+#include <span>
 #include <string>
+#include <vector>
 
 export module Soup.BuildDatabase;
+
+import :PackageLoadState;
 
 import json11;
 import Opal;
@@ -15,192 +19,127 @@ import Soup.Core;
 using namespace Opal;
 using namespace Soup::Core;
 
-json11::Json ConvertToJson(const PackageGraphLookupMap &lookup) {
-	auto result = json11::Json::array();
+json11::Json GeneratePackageBuildSet(FileSystemState &fileSystemState,
+									 PackageProvider &packageProvider,
+									 int graphId, int packageId) {
+	auto operationGraph =
+		LoadPackage(fileSystemState, packageProvider, graphId, packageId);
 
-	for (const auto &value : lookup) {
-		auto jsonValue = json11::Json::object({
-			{"Id", value.second.Id},
-			{"RootPackageId", value.second.RootPackageId},
-			// TODO : { "GlobalParameters", value.second.GlobalParameters },
-		});
-
-		result.push_back(std::move(jsonValue));
+	auto translationUnits = json11::Json::array();
+	if (operationGraph.has_value()) {
+		// operationGraph.value()
+		translationUnits.push_back(json11::Json::array({
+			json11::Json::object({
+				{
+					"arguments",
+					json11::Json::array({
+						"/path/to/compiler",
+						"...",
+					}),
+				},
+				{"baseline-arguments", json11::Json::array({
+										   "...",
+									   })},
+				{"local-arguments", json11::Json::array({
+										"...",
+									})},
+				{"object", "CMakeFiles/target.dir/source.cxx.o"},
+				{"private", true},
+				{"provides", json11::Json::object({
+								 {"importable", "path/to/bmi"},
+							 })},
+				{"requires", json11::Json::array()},
+				{"source", "path/to/source.cxx"},
+				{"work-directory", "/path/to/working/directory"},
+				{"visible-sets", json11::Json::array()},
+			}),
+		}));
+		// packageTabComponents.push_back(operationsView);
 	}
 
-	return result;
+	return translationUnits;
 }
 
-json11::Json ConvertToJson(const PackageLookupMap &lookup) {
-	auto result = json11::Json::array();
+void ConvertToJson(FileSystemState &fileSystemState,
+				   PackageProvider &packageProvider, int packageId,
+				   std::set<int> &knownPackages, json11::Json::array &sets) {
+	auto &packageInfo = packageProvider.GetPackageInfo(packageId);
+	auto rootPackageGraphId = packageProvider.GetRootPackageGraphId();
+	knownPackages.insert(packageInfo.Id);
 
-	for (const auto &value : lookup) {
-		auto dependencies = json11::Json::object();
-		for (const auto &dependency : value.second.Dependencies) {
-			auto dependencyChildren = json11::Json::array();
-			for (const auto &dependencyChild : dependency.second) {
-				auto jsonChildValue = json11::Json::object({
-					{"OriginalReference",
-					 dependencyChild.OriginalReference.ToString()},
-					{"IsSubGraph", dependencyChild.IsSubGraph},
-					{"PackageId", dependencyChild.PackageId},
-					{"PackageGraphId", dependencyChild.PackageGraphId},
-				});
-				dependencyChildren.push_back(std::move(jsonChildValue));
+	for (auto &[dependencyType, dependencyTypeSet] : packageInfo.Dependencies) {
+		for (auto &dependency : dependencyTypeSet) {
+			// Stop at the edge of the graph and ignore duplicates
+			if (!dependency.IsSubGraph &&
+				!knownPackages.contains(dependency.PackageId)) {
+				ConvertToJson(fileSystemState, packageProvider,
+							  dependency.PackageId, knownPackages, sets);
 			}
-
-			dependencies.emplace(dependency.first,
-								 std::move(dependencyChildren));
 		}
-
-		auto ownerValue = json11::Json();
-		if (value.second.Name.HasOwner()) {
-			ownerValue = value.second.Name.GetOwner();
-		}
-
-		auto jsonValue = json11::Json::object({
-			{"Id", value.second.Id},
-			{"Name", value.second.Name.GetName()},
-			{"Owner", std::move(ownerValue)},
-			{"IsPrebuilt", value.second.IsPrebuilt()},
-			{"PackageRoot", value.second.PackageRoot.ToString()},
-			{"Dependencies", std::move(dependencies)},
-		});
-		result.push_back(std::move(jsonValue));
 	}
 
-	return result;
-}
+	auto translationUnits = GeneratePackageBuildSet(
+		fileSystemState, packageProvider, rootPackageGraphId, packageId);
 
-json11::Json
-ConvertToJson(const PackageTargetDirectories &packageGraphTargetDirectories) {
-	auto result = json11::Json::object();
+	auto set = json11::Json::object({
+		{"family-name", packageInfo.Name.ToString()},
+		{"name", packageInfo.Name.ToString() + "@Debug"},
+		{"translation-units", std::move(translationUnits)},
+	});
 
-	for (const auto &[packageGraphId, packageTargetDirectories] :
-		 packageGraphTargetDirectories) {
-		auto packageTargetDirectoriesResult = json11::Json::object();
-		for (const auto &[packageId, targetDirectory] :
-			 packageTargetDirectories) {
-			packageTargetDirectoriesResult.emplace(std::to_string(packageId),
-												   targetDirectory.ToString());
-		}
-
-		result.emplace(std::to_string(packageGraphId),
-					   std::move(packageTargetDirectoriesResult));
-	}
-
-	return result;
-}
-
-Value JsonToValue(const json11::Json &json);
-
-ValueList JsonToValueList(const json11::Json::array &json) {
-	auto result = ValueList();
-	for (const auto &value : json) {
-		result.push_back(JsonToValue(value));
-	}
-
-	return result;
-}
-
-ValueTable JsonToValueTable(const json11::Json::object &json) {
-	auto result = ValueTable();
-	for (const auto &[key, value] : json) {
-		result.emplace(key, JsonToValue(value));
-	}
-
-	return result;
-}
-
-Value JsonToValue(const json11::Json &json) {
-	switch (json.type()) {
-		case json11::Json::NUL:
-			throw std::runtime_error("Null is not supported as value");
-		case json11::Json::NUMBER:
-			return Value(json.number_value());
-		case json11::Json::BOOL:
-			return Value(json.bool_value());
-		case json11::Json::STRING:
-			return Value(json.string_value());
-		case json11::Json::ARRAY:
-			return Value(JsonToValueList(json.array_items()));
-		case json11::Json::OBJECT:
-			return Value(JsonToValueTable(json.object_items()));
-		default:
-			throw std::runtime_error("Unknown json type");
-	}
+	sets.push_back(std::move(set));
 }
 
 export std::string LoadBuildGraphContent(const Path &workingDirectory) {
-	try {
-		// Setup the filter
-		auto defaultTypes =
-			// static_cast<uint32_t>(TraceEventFlag::Diagnostic) |
-			// static_cast<uint32_t>(TraceEventFlag::Information) |
-			// static_cast<uint32_t>(TraceEventFlag::HighPriority) |
-			static_cast<uint32_t>(TraceEventFlag::Warning) |
-			static_cast<uint32_t>(TraceEventFlag::Error) |
-			static_cast<uint32_t>(TraceEventFlag::Critical);
-		auto filter = std::make_shared<EventTypeFilter>(
-			static_cast<TraceEventFlag>(defaultTypes));
+	// Setup the filter
+	auto defaultTypes =
+		// static_cast<uint32_t>(TraceEventFlag::Diagnostic) |
+		// static_cast<uint32_t>(TraceEventFlag::Information) |
+		// static_cast<uint32_t>(TraceEventFlag::HighPriority) |
+		static_cast<uint32_t>(TraceEventFlag::Warning) |
+		static_cast<uint32_t>(TraceEventFlag::Error) |
+		static_cast<uint32_t>(TraceEventFlag::Critical);
+	auto filter = std::make_shared<EventTypeFilter>(
+		static_cast<TraceEventFlag>(defaultTypes));
 
-		// Setup the console listener
-		Log::RegisterListener(std::make_shared<ConsoleTraceListener>(
-			"Log", filter, false, false));
+	// Setup the console listener
+	Log::RegisterListener(
+		std::make_shared<ConsoleTraceListener>("Log", filter, false, false));
 
-		// Setup the real services
-		System::ISystem::Register(std::make_shared<System::STLSystem>());
-		System::IFileSystem::Register(
-			std::make_shared<System::STLFileSystem>());
+	// Setup the real services
+	System::ISystem::Register(std::make_shared<System::STLSystem>());
+	System::IFileSystem::Register(std::make_shared<System::STLFileSystem>());
 
 // Platform specific defaults
 #if defined(_WIN32)
-		auto hostPlatform = "Windows";
+	auto hostPlatform = "Windows";
 #elif defined(__linux__)
-		auto hostPlatform = "Linux";
+	auto hostPlatform = "Linux";
 #else
 #error "Unknown Platform"
 #endif
 
-		auto globalParameters = ValueTable();
+	auto globalParameters = ValueTable();
 
-		// Load user config state
-		auto userDataPath = Build::Constants::GetSoupUserDataPath();
+	// Load user config state
+	auto userDataPath = Build::Constants::GetSoupUserDataPath();
 
-		auto recipeCache = RecipeCache();
+	auto recipeCache = RecipeCache();
 
-		auto packageProvider = Build::LoadBuildGraph(
-			workingDirectory, std::nullopt, globalParameters, userDataPath,
-			hostPlatform, recipeCache);
+	auto packageProvider =
+		Build::LoadBuildGraph(workingDirectory, std::nullopt, globalParameters,
+							  userDataPath, hostPlatform, recipeCache);
 
-		auto packageGraphs =
-			ConvertToJson(packageProvider.GetPackageGraphLookup());
-		auto packages = ConvertToJson(packageProvider.GetPackageLookup());
-		auto packageTargetDirectories =
-			ConvertToJson(packageProvider.GetPackageTargetDirectories());
+	auto fileSystemState = FileSystemState();
 
-		json11::Json jsonGraphResult = json11::Json::object({
-			{"RootPackageGraphId", packageProvider.GetRootPackageGraphId()},
-			{"PackageGraphs", std::move(packageGraphs)},
-			{"Packages", std::move(packages)},
-			{"PackageTargetDirectories", std::move(packageTargetDirectories)},
-		});
+	auto &packageGraph = packageProvider.GetRootPackageGraph();
+	auto sets = json11::Json::array();
+	auto knownPackages = std::set<int>();
+	ConvertToJson(fileSystemState, packageProvider, packageGraph.RootPackageId,
+				  knownPackages, sets);
 
-		json11::Json jsonResult = json11::Json::object(
-			{{"version", 1}, {"revision", 0}, {"sets", json11::Json::array()}});
-		auto value = jsonResult.dump();
-		return value;
-	} catch (const HandledException &ex) {
-		json11::Json jsonResult = json11::Json::object({
-			{"Message", std::format("HANDLED ERROR: {0}", ex.GetExitCode())},
-			{"IsSuccess", false},
-		});
-		return jsonResult.dump();
-	} catch (const std::exception &ex) {
-		json11::Json jsonResult = json11::Json::object({
-			{"Message", ex.what()},
-			{"IsSuccess", false},
-		});
-		return jsonResult.dump();
-	}
+	json11::Json jsonResult = json11::Json::object(
+		{{"version", 1}, {"revision", 0}, {"sets", std::move(sets)}});
+	auto value = jsonResult.dump();
+	return value;
 }
---
+++ code/tools/build-database/package-load-state.cpp
@@ -0,0 +1,51 @@
+﻿// <copyright file="package-load-state.cpp" company="Soup">
+// Copyright (c) Soup. All rights reserved.
+// </copyright>
+
+module;
+
+#include <optional>
+
+export module Soup.BuildDatabase:PackageLoadState;
+
+import Opal;
+import Soup.Core;
+
+using namespace Opal;
+using namespace Soup::Core;
+
+export std::optional<OperationGraph>
+LoadPackage(FileSystemState &fileSystemState, PackageProvider &packageProvider,
+			int graphId, int packageId) {
+	// Get the target directory
+	const auto &targetDirectory =
+		packageProvider.GetTargetDirectory(graphId, packageId);
+
+	auto soupTargetDirectory = targetDirectory + Path("./.soup/");
+
+	auto generatePhase1ResultFile =
+		soupTargetDirectory + Build::Constants::GeneratePhase1ResultFileName();
+	auto generatePhase1Result = GenerateResult();
+	bool generatePhase1IsPreprocessor = false;
+	if (GenerateResultManager::TryLoadState(
+			generatePhase1ResultFile, generatePhase1Result, fileSystemState)) {
+		generatePhase1IsPreprocessor = generatePhase1Result.HasPreprocessor();
+		if (generatePhase1IsPreprocessor) {
+			// Check for the optional evaluate graph if the initial phase was
+			// preprocessor
+			auto generatePhase2ResultFile =
+				soupTargetDirectory +
+				Build::Constants::GeneratePhase2ResultFileName();
+			auto generatePhase2Result = OperationGraph();
+			if (OperationGraphManager::TryLoadState(generatePhase2ResultFile,
+													generatePhase2Result,
+													fileSystemState)) {
+				return generatePhase2Result;
+			}
+		} else {
+			return generatePhase1Result.GetGraph();
+		}
+	}
+
+	return std::nullopt;
+}
